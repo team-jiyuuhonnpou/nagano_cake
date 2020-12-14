cb2177eb416946af608fddb95af278045f0f9331
EC2 Default User <ec2-user@ip-172-31-30-84.us-east-2.compute.internal>
2020-12-14 08:34:41 +0000

    [add]routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #homes
+  root to: 'homes#top'
+  get '/about' => 'homes#about'
+
+  #admins
+  namespace :admins do
+    #devise_for :admins
+    resources :orders, only: [:index, :show, :update] do
+      patch '/order_items/:id' => 'order_items#update'
+    end
+    resources :items, except: [:destroy]
+    resources :customers, only: [:index, :show, :edit, :update]
+    patch '/custamers/:id/hide' => 'customers#hide'
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
+    get '/homes/top' => 'homes#top'
+  end
+
+  #custamer
+  namespace :customers do
+    #devise_for :customers
+    resources :orders, only: [:new, :create, :index, :show]
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/thanks' => 'orders#thanks'
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update]
+    get '/customers/unsubscribe' => 'custamers#unsubscribe'
+    patch '/customers/hide' => 'customers#hide'
+    resources :deliveries, only: [:index, :create, :destroy, :edit, :update]
+    resources :cart_items, only: [:index, :create, :destroy, :update]
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+  end
+
 end
