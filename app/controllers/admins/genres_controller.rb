5f600c49526e7cd712f50c2afadcf9e2c5caa97e
EC2 Default User <ec2-user@ip-172-31-30-84.us-east-2.compute.internal>
2020-12-20 03:16:12 +0000

    [add]layouts/views

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,6 +1,6 @@
 class Admins::GenresController < ApplicationController
 
-  #before_action :authenticate_admin!
+  #before_action :authenticate_admin!, expect: [:show]
 
   def index
     @genre = Genre.new
@@ -19,19 +19,30 @@ class Admins::GenresController < ApplicationController
     end
   end
 
+  def show
+    @items = Item.all
+    @items = Item.page(params[:page]).reverse_order.per(10)
+    @genre = Genre.find(params[:id])
+    @genres = @genre.items.order(creates_at: :desc).all.page(params[:page]).per(5)
+  end
+  
   def edit
     @genre = Genre.find(params[:id])
   end
 
   def update
     @genre = Genre.find(params[:id])
-    #フラグについて
+    if @genre.update(genre_params)
+      redirect_to admins_genres_path
+    else
+      render :edit
+    end
   end
-  
-  
+
+
   private
   def genre_params
     params.require(:genre).permit(:name, :is_active)
   end
-  
+
 end
