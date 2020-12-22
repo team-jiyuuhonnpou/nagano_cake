16bef0ff4ba66d3cf1b3796f90a292fd1500e851
takahirokobayashi-42 <t.kobayashi5820@outlook.jp>
2020-12-22 02:56:12 +0000

    order item css

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -6,7 +6,7 @@ class Customers::OrdersController < ApplicationController
     @order = Order.new
     @deliveries = current_customer.deliveries
   end
-  
+
   def confirm
     @order = Order.new(order_params)
     @cart_items = current_customer.cart_items
@@ -42,16 +42,16 @@ class Customers::OrdersController < ApplicationController
     @order.customer_id = current_customer.id
 
     if @order.save
-      @cart_items = current_customer.cart_items
-      @order_item = OrderItem.new
       @cart_items.each do |cart_item|
+      @order_item = OrderItem.new
       @order_item.item_id = cart_item.item_id
       @order_item.amount = cart_item.amount
       @order_item.price = cart_item.item.non_taxed_price
       @order_item.order_id = @order.id
+      
       end
-      @order_item.save
-  
+      
+
       @cart_items.destroy_all
       redirect_to  customers_orders_thanks_path
     end
@@ -68,8 +68,11 @@ class Customers::OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find(params[:id])
+    # @order = Order.find(params[:id])
+    # @order_items = @order.order_items
+     @order = Order.find(params[:id])
     @order_items = @order.order_items
+    @total_price = @order_items.sum(:price)
   end
 
   private
