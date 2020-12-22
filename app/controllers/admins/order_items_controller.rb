6cfe1d465262015aaa496033e8ff6e0d35e49456
takahiro1202 <tk.tennis.w@gmail.com>
2020-12-22 02:40:23 +0000

    [Add]admins item_view order_view

--- app/controllers/admins/order_items_controller.rb
+++ app/controllers/admins/order_items_controller.rb
@@ -1,23 +1,25 @@
 class Admins::OrderItemsController < ApplicationController
-  
+
   #before_action :authenticate_admin!
-  
+
   def update
     @order_item = OrderItem.find(params[:id])
-    @order = Order.find(params[:order_id])
-    @order_items.update(order_item_params)
-     if @order_item.production_status == 2
+    @order = @order_item.order
+    @order_item.update(order_item_params)
+      if @order_item.production_status == "製作中"
         @order.update(status: 2)
-     elsif @order_item.production_status == 3
+      else
+        item = @order.order_items.select(:production_status).distinct
+        item == 1 && @order_item.production_status == "製作完了"
         @order.update(status: 3)
-     end
+      end
       redirect_to  admins_order_path(@order)
       #render相談
   end
-  
+
   private
   def order_item_params
     params.require(:order_item).permit(:production_status)
   end
-  
+
 end
