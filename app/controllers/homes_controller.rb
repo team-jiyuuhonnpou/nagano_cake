b0f177dbd4f74b1899c305a37c9099496738f76c
Yasuaki <yasu5293koh@gmail.com>
2020-12-22 02:34:54 +0000

    yasu_view

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,9 @@
 class HomesController < ApplicationController
   def top
-    @items = Item.all
+    @items = Item.limit(4)
+    @genres = Genre.where(is_active: 1)
   end
-
+  
   def about
   end
 end
