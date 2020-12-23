3b78daf2b0d5b13644d05f5aa9e6d4e062de6903
Yasuaki <yasu5293koh@gmail.com>
2020-12-23 02:50:00 +0000

    check

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,5 +1,9 @@
 class CartItem < ApplicationRecord
   belongs_to :item
   belongs_to :customer
-
+  
+  validates :item_id, :customer_id, :amount, presence: true
+  # 1以上の整数のみ許可
+  validates :amount, numericality: { only_integer: true, greater_than: 0 }
+  
 end
