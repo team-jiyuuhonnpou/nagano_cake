b70089510557be220212b0dc6ca9de35340fec70
takahiro1202 <tk.tennis.w@gmail.com>
2020-12-24 07:30:58 +0000

    [Add]rspec

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :item do
-    image { "no-image.png" }
+    image { File.open('./app/assets/images/no-image.png') }
     item_name { "ロールケーキ" }
     explanation { "ロールケーキ" }
     non_taxed_price { 100 }
--- spec/requests/takahiro_spec.rb
+++ spec/requests/takahiro_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe '管理者ログイン' do
+RSpec.describe '管理者' do
 
   before do
     @admin = FactoryBot.create(:admin)
+    @customer = FactoryBot.create(:customer)
+    @item = FactoryBot.create(:item)
   end
 
   it "トップ画面が表示される" do
@@ -20,5 +22,23 @@ RSpec.describe '管理者ログイン' do
     click_on "注文履歴一覧"
     expect(page).to have_current_path admins_orders_path
   end
+
+  it "注文ステータスが「入金確認」、製作ステータスが「製作待ち」に更新される" do
+    sign_in @customer
+    visit customers_item_path(@item.id)
+    select "3", from: 'cart_item[amount]'
+    click_button 'カートに入れる'
+    click_button '情報入力に進む'
+    choose 'クレジットカード'
+    choose 'ご自身の住所'
+    click_button '確認画面に進む'
+    click_button '購入を確定する'
+    sign_in @admin
+    visit admins_order_path(order_id assigns(:order).id)
+    select '入金確認', form: 'order[status]'
+    click_button 'status_button'
+    expect(page).to have_content '製作待ち'
+  end
+
 
 end
